0c4929d3d859395ba288339e36a574aee6723071
Lucas Palmieri <lucas.palmieri@superare.co>
2020-01-07 10:34:22 -0300

    upload react static files

---
+++ precache-manifest.7f4231df691ffdbe2272ffc9357dc81c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3ef0a1f70d551ac3b7474406f0e783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43da0e9dc7c9f7e1c49a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0d98e97add8a6c08614a",
+    "url": "/static/css/main.e994fef4.chunk.css"
+  },
+  {
+    "revision": "43da0e9dc7c9f7e1c49a",
+    "url": "/static/js/2.89a76234.chunk.js"
+  },
+  {
+    "revision": "0d98e97add8a6c08614a",
+    "url": "/static/js/main.c25ed110.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f5429230c282ad289390bb38db4b8fd",
+    "url": "/static/media/profile.0f542923.jpg"
+  }
+]);
