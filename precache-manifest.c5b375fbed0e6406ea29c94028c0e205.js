bce5db870a4430e754aeb3c1791f84462a64eaf2
Lucas Palmieri <lucas.palmieri@superare.co>
2020-01-07 10:40:25 -0300

    new upload static react files

---
+++ precache-manifest.c5b375fbed0e6406ea29c94028c0e205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17211cdaf58abbbf35d11c837ef5a45d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "50bbd70a59b984c15d2e",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "970a504d596031bc2d55",
+    "url": "./static/css/main.e994fef4.chunk.css"
+  },
+  {
+    "revision": "50bbd70a59b984c15d2e",
+    "url": "./static/js/2.2c2eaf0d.chunk.js"
+  },
+  {
+    "revision": "970a504d596031bc2d55",
+    "url": "./static/js/main.9393eb02.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0f5429230c282ad289390bb38db4b8fd",
+    "url": "./static/media/profile.0f542923.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f4231df691ffdbe2272ffc9357dc81c.js"
+  "./precache-manifest.c5b375fbed0e6406ea29c94028c0e205.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
